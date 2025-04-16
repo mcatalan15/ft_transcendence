33cfa0b79c46a963356ca896422be65368031dc5
nponchon <nponchon@student.42.fr>
2025-04-16 17:12:09 +0200

    css coming your way

--- containers/frontend/tailwind.config.js
+++ containers/frontend/tailwind.config.js
@@ -1,11 +1,22 @@
 module.exports = {
 	content: [
 	  "./src/**/*.{html,ts}",
-	  "./public/index.html"
+	  "./public/index.html",
 	],
 	theme: {
 	  extend: {},
 	},
 	plugins: [],
+
+	// Optional purge
+	purge: {
+	  content: [
+		"./src/**/*.{html,ts}",
+		"./public/index.html"
+	  ],
+	  options: {
+		safelist: ['body', 'html'],
+	  },
+	},
   }
   
