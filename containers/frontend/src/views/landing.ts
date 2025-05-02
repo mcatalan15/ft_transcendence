b6275ba4be91ecae6a269f46542452c3b622d034
eferre-m <151768057+eferre-m@users.noreply.github.com>
2025-05-02 19:40:08 +0200

    landing page with buttons

--- containers/frontend/src/views/landing.ts
+++ containers/frontend/src/views/landing.ts
@@ -2,12 +2,24 @@ export function showLanding(container: HTMLElement): void {
 	const landingDiv = document.createElement('div');
 
 	landingDiv.innerHTML = `
-		<div class="h-screen flex flex-col items-center justify-center bg-neutral-900 text-amber-50">
+		<div class="h-screen relative bg-neutral-900 text-amber-50 flex flex-col items-center justify-center">
+
+			<div class="absolute top-6 w-full flex justify-center gap-x-4">
+				<button onclick="navigate('/login')"
+					class="bg-amber-50 text-neutral-900 px-4 py-2 rounded">
+					Sign in
+				</button>
+				<button onclick="navigate('/register')"
+					class="border border-amber-50 text-amber-50 px-4 py-2 rounded">
+					Sign up
+				</button>
+			</div>
+
 			<div class="text-9xl font-anatol flex items-center space-x-4">
-				<span class="">P</span>
+				<span>P</span>
 				<button onclick="navigate('/login')"
 					class="w-16 h-16 bg-amber-400 rounded-full animate-bounce shadow-lg hover:scale-110 transition duration-300 cursor-zoom-in"
-					title="Enter">
+					title="Entrar">
 				</button>
 				<span>NG</span>
 			</div>
