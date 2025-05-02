a0a36566de80249470922d7e954cfa2fbb354e5d
Eva <e.fmur@gmail.com>
2025-05-02 16:54:20 +0200

    (first) landing view: pong  done

--- containers/frontend/src/views/landing.ts
+++ containers/frontend/src/views/landing.ts
@@ -2,14 +2,12 @@ export function showLanding(container: HTMLElement): void {
 	const landingDiv = document.createElement('div');
 
 	landingDiv.innerHTML = `
-		<div class="h-screen flex flex-col items-center justify-center bg-black text-white">
-			<!-- Palabra PONG sin la "O", con la pelota en su lugar -->
+		<div class="h-screen flex flex-col items-center justify-center bg-neutral-900 text-amber-50">
 			<div class="text-9xl font-anatol flex items-center space-x-4">
 				<span class="">P</span>
-				<!-- Pelota animada como botón -->
 				<button onclick="navigate('/login')"
-					class="w-16 h-16 bg-white rounded-full animate-bounce shadow-lg hover:scale-110 transition duration-300"
-					title="Entrar al juego">
+					class="w-16 h-16 bg-amber-400 rounded-full animate-bounce shadow-lg hover:scale-110 transition duration-300 cursor-zoom-in"
+					title="Enter">
 				</button>
 				<span>NG</span>
 			</div>
