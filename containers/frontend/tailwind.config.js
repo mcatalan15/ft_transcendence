ec1000f04e4cc4a3a52f53d332ca0755e2d55c24
nponchon <nponchon@student.42.fr>
2025-06-03 12:28:26 +0200

    Revert "Merge branch 'eva-SPA3' into develop4"
    This reverts commit 15cc6da56dba4f81e0ffd12ce6813487570753ad, reversing
    changes made to a060e421f2f20112df286b4c6e11e77a6f19bf25.

--- containers/frontend/tailwind.config.js
+++ containers/frontend/tailwind.config.js
@@ -8,41 +8,20 @@ module.exports = {
 		fontFamily: {
 			anatol:['"anatol-mn"', 'sans-serif'],
 		},
-		animation: {
-			'float-x': 'floatX 4s ease-in-out infinite',
-			'float-y': 'floatY 6s ease-in-out infinite',
-		  },
-		  keyframes: {
-			floatX: {
-			  '0%, 100%': { transform: 'translateX(0)' },
-			  '50%': { transform: 'translateX(20px)' },
-			},
-			floatY: {
-			  '0%, 100%': { transform: 'translateY(0)' },
-			  '50%': { transform: 'translateY(-20px)' },
-			},
-		  },
-		},
-	  },
-	  plugins: [],
-	}
-
-  export default {
-  darkMode: 'class',
-  content: ['./index.html', './src/**/*.{ts,js}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
-
-//module.exports = {
-//	content: ['./src/**/*.{ts,tsx,html}'],
-//	theme: {
-/*	  extend: {
 	  },
 	},
 	plugins: [],
-  };*/
+
+
+	// Optional purge
+	purge: {
+	  content: [
+		"./src/**/*.{html,ts}",
+		"./public/index.html"
+	  ],
+	  options: {
+		safelist: ['body', 'html'],
+	  },
+	},
+  }
   
