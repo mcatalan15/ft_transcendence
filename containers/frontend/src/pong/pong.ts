d810801791a36bbbd5702e821d66f15f02cd8527
nponchon <nponchon@student.42.fr>
2025-06-26 15:05:30 +0200

    merged from marc-develop5 successful?

--- containers/frontend/src/pong/pong.ts
+++ containers/frontend/src/pong/pong.ts
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   pong.ts                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hmunoz-g <hmunoz-g@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nponchon <nponchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 11:29:12 by hmunoz-g          #+#    #+#             */
-/*   Updated: 2025/06/20 18:01:51 by hmunoz-g         ###   ########.fr       */
+/*   Updated: 2025/06/26 14:35:18 by nponchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 import { Application } from "pixi.js";
 import { Menu } from './menu/Menu';
 
-export async function initGame() {
+export async function initGame(container: HTMLElement) {
   const app = new Application();
   await app.init({
     background: '#171717',
@@ -27,8 +27,6 @@ export async function initGame() {
   //const language = localStorage.getItem('i18nextLng') || 'en';
   const language = 'en';
 
-  const container = document.getElementById("game-container");
-  if (!container) throw new Error("game-container not found!");
   container.appendChild(app.canvas);
   
   const menu = new Menu(app, language);
