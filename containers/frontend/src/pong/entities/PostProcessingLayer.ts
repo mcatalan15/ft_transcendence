6987c100528344e1318a63b9f121091db4a31ea6
Hugo Munoz Gris <hmunoz-g@cbr5s1.42barcelona.com>
2025-07-22 10:17:37 +0200

    optimized filters for firefox

--- containers/frontend/src/pong/entities/PostProcessingLayer.ts
+++ containers/frontend/src/pong/entities/PostProcessingLayer.ts
@@ -6,7 +6,7 @@
 /*   By: hmunoz-g <hmunoz-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 17:47:20 by hmunoz-g          #+#    #+#             */
-/*   Updated: 2025/07/21 20:33:55 by hmunoz-g         ###   ########.fr       */
+/*   Updated: 2025/07/22 10:17:21 by hmunoz-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,80 +25,81 @@ export class PostProcessingLayer extends Entity {
         const container = new Container();
         this.addComponent(new RenderComponent(container));
 
+        const isFirefox = game.isFirefox;
+
         const advancedBloom = new AdvancedBloomFilter({
-            threshold: 0.7,
-            bloomScale: 0.2,
+            threshold: isFirefox ? 0.8 : 0.7,
+            bloomScale: isFirefox ? 0.15 : 0.2,
             brightness: 1,
-            blur: 1,
-            quality: 10,
-            pixelSize: 0.5,
+            blur: isFirefox ? 0.8 : 1,
+            quality: isFirefox ? 6 : 10,
+            pixelSize: isFirefox ? 0.8 : 0.5,
         });
 
         const crtFilter = new CRTFilter({
             curvature: (game.width * 0.0005 + game.height * 0.0005) / 2,
-            lineWidth: 0.1,
-            lineContrast: 0.2,
+            lineWidth: isFirefox ? 0.08 : 0.1,
+            lineContrast: isFirefox ? 0.15 : 0.2,
             verticalLine: false,
-            noise: 0.1, 
-            noiseSize: 0.5,
+            noise: isFirefox ? 0.07 : 0.1,
+            noiseSize: isFirefox ? 0.7 : 0.5,
             seed: Math.random(),
-            vignetting: 0.45,
-            vignettingAlpha: 0.6,
-            vignettingBlur: 0.1,
+            vignetting: isFirefox ? 0.35 : 0.45,
+            vignettingAlpha: isFirefox ? 0.4 : 0.6,
+            vignettingBlur: isFirefox ? 0.05 : 0.1,
             time: 0 
         });
 
-
         const bulgePinch = new BulgePinchFilter({
             center: new Point(0.5, 0.5), 
-            radius: Math.min(game.width, game.height) * 1.6,
-            strength: (1 / game.width / game.height) * 70000,
+            radius: Math.min(game.width, game.height) * (isFirefox ? 1.4 : 1.6),
+            strength: (1 / game.width / game.height) * (isFirefox ? 50000 : 70000),
         });
 
         const rgbSplit = new RGBSplitFilter({
-            red:   new Point(-0.5, 0),
-            green: new Point(0, 0.5),
-            blue:  new Point(0.5, -0.5),
+            red:   new Point(isFirefox ? -0.3 : -0.5, 0),
+            green: new Point(0, isFirefox ? 0.3 : 0.5),
+            blue:  new Point(isFirefox ? 0.3 : 0.5, isFirefox ? -0.3 : -0.5),
         });
 
         const glow = new GlowFilter({
-            alpha: 0.1,
+            alpha: isFirefox ? 0.08 : 0.1,
             color: GAME_COLORS.white,
-            distance: 10,
+            distance: isFirefox ? 7 : 10,
             innerStrength: 0,
             knockout: false,
-            outerStrength: 2,
-            quality: 0.1,
+            outerStrength: isFirefox ? 1.5 : 2,
+            quality: isFirefox ? 0.05 : 0.1,
         });
 
         const depthLineCRTFilter = new CRTFilter({
             curvature: (game.width * 0.0005 + game.height * 0.0005) / 2,
-            lineWidth: 0.1,
-            lineContrast: 0.2,
+            lineWidth: isFirefox ? 0.08 : 0.1,
+            lineContrast: isFirefox ? 0.15 : 0.2,
             verticalLine: false,
             noise: 0,
             noiseSize: 0.5,
             seed: Math.random(),
-            vignetting: 0.5,
-            vignettingAlpha: 1,
-            vignettingBlur: 0.2,
+            vignetting: isFirefox ? 0.4 : 0.5,
+            vignettingAlpha: isFirefox ? 0.8 : 1,
+            vignettingBlur: isFirefox ? 0.15 : 0.2,
             time: 0
         });
 
         const depthLineGlow = new GlowFilter({
-            alpha: 0.1,
+            alpha: isFirefox ? 0.08 : 0.1,
             color: GAME_COLORS.white,
-            distance: 10,
+            distance: isFirefox ? 7 : 10,
             innerStrength: 0,
             knockout: false,
-            outerStrength: 1,
-            quality: 0.1,
+            outerStrength: isFirefox ? 0.8 : 1,
+            quality: isFirefox ? 0.05 : 0.1,
         });
 
         const powerupCRT = new CRTFilter({
             curvature: (game.width * 0.0005 + game.height * 0.0005) / 2,
-            lineWidth: 0.1,
-            lineContrast: 0.2,
+            lineWidth: isFirefox ? 0.08 : 0.1,
+            lineContrast: isFirefox ? 0.15 : 0.2,
             verticalLine: false,
             noise: 0.,
             noiseSize: 0.5,
@@ -110,62 +111,62 @@ export class PostProcessingLayer extends Entity {
         });
 
         const powerupGlow = new GlowFilter({
-            alpha: 0.2,
+            alpha: isFirefox ? 0.15 : 0.2,
             color: GAME_COLORS.marine,
-            distance: 10,
-            innerStrength: 3,
+            distance: isFirefox ? 7 : 10,
+            innerStrength: isFirefox ? 2 : 3,
             knockout: false,
-            outerStrength: 2,
-            quality: 0.1,
+            outerStrength: isFirefox ? 1.5 : 2,
+            quality: isFirefox ? 0.05 : 0.1,
         });
 
         const powerdownGlow = new GlowFilter({
-            alpha: 0.2,
+            alpha: isFirefox ? 0.15 : 0.2,
             color: GAME_COLORS.marine,
-            distance: 10,
-            innerStrength: 3,
+            distance: isFirefox ? 7 : 10,
+            innerStrength: isFirefox ? 2 : 3,
             knockout: false,
-            outerStrength: 2,
-            quality: 0.1,
+            outerStrength: isFirefox ? 1.5 : 2,
+            quality: isFirefox ? 0.05 : 0.1,
         });
 
         const ballChangeGlow = new GlowFilter({
-            alpha: 0.2,
+            alpha: isFirefox ? 0.15 : 0.2,
             color: GAME_COLORS.marine,
-            distance: 10,
-            innerStrength: 3,
+            distance: isFirefox ? 7 : 10,
+            innerStrength: isFirefox ? 2 : 3,
             knockout: false,
-            outerStrength: 2,
-            quality: 0.1,
+            outerStrength: isFirefox ? 1.5 : 2,
+            quality: isFirefox ? 0.05 : 0.1,
         });
 
         const powerupDropShadow = new DropShadowFilter({
-            alpha: 0.75,
-            blur: 1,
+            alpha: isFirefox ? 0.6 : 0.75,
+            blur: isFirefox ? 0.8 : 1,
             color: GAME_COLORS.green,
-            offset: {x: 4,y: 4},
-            pixelSize: {x:1,y:1},
-            quality: 4,
+            offset: {x: isFirefox ? 3 : 4, y: isFirefox ? 3 : 4},
+            pixelSize: {x: 1, y: 1},
+            quality: isFirefox ? 2 : 4,
             resolution: 1,
         });
         
         const powerdownDropShadow = new DropShadowFilter({
-            alpha: 0.75,
-            blur: 1,
+            alpha: isFirefox ? 0.6 : 0.75,
+            blur: isFirefox ? 0.8 : 1,
             color: GAME_COLORS.red,
-            offset: {x: 4,y: 4},
-            pixelSize: {x:1,y:1},
-            quality: 4,
+            offset: {x: isFirefox ? 3 : 4, y: isFirefox ? 3 : 4},
+            pixelSize: {x: 1, y: 1},
+            quality: isFirefox ? 2 : 4,
             resolution: 1,
         });
 
         const ballChangeDropShadow = new DropShadowFilter({
-            alpha: 0.75,
-            blur: 1,
+            alpha: isFirefox ? 0.6 : 0.75,
+            blur: isFirefox ? 0.8 : 1,
             color: GAME_COLORS.brown,
-            offset: {x: 4,y: 4},
-            pixelSize: {x:1,y:1},
-            quality: 4,
+            offset: {x: isFirefox ? 3 : 4, y: isFirefox ? 3 : 4},
+            pixelSize: {x: 1, y: 1},
+            quality: isFirefox ? 2 : 4,
             resolution: 1,
         });
 
@@ -176,19 +177,39 @@ export class PostProcessingLayer extends Entity {
             red: {x: 0.5, y: 0.5},
             direction: 0,
             fillMode: 1,
-            offset : 2,
-            sampleSize: 512,
+            offset: isFirefox ? 1 : 2,
+            sampleSize: isFirefox ? 256 : 512,
             seed: 0,
-            slices: 200,
+            slices: isFirefox ? 100 : 200,
         });
 
         const powerupMotionBlur = new MotionBlurFilter({
-           kernelSize: 5,
-           offset: 1,
-           velocity: {x: 0, y:0},
+           kernelSize: isFirefox ? 3 : 5,
+           offset: isFirefox ? 0.7 : 1,
+           velocity: {x: 0, y: 0},
         });
 
-        game.visualRoot.filters = [glow, advancedBloom, bulgePinch, crtFilter, rgbSplit];
+        if (isFirefox) {
+            game.visualRoot.filters = [glow, advancedBloom, crtFilter];
+
+            game.renderLayers.background.filters = [depthLineGlow, depthLineCRTFilter];
+
+            game.renderLayers.powerup.filters = [powerupGlow, advancedBloom, powerupCRT, powerupDropShadow];
+            game.renderLayers.powerupGlitched.filters = [powerupGlow, advancedBloom, powerupCRT, powerupDropShadow, powerdownGlitch];
+            game.renderLayers.powerdown.filters = [powerdownGlow, advancedBloom, powerupCRT, powerdownDropShadow, powerdownGlitch];
+            game.renderLayers.ballChange.filters = [ballChangeGlow, advancedBloom, powerupCRT, ballChangeDropShadow];
+            game.renderLayers.crossCut.filters = [powerupGlow, advancedBloom, powerupCRT, powerupDropShadow];
+        } else {
+            game.visualRoot.filters = [glow, advancedBloom, bulgePinch, crtFilter, rgbSplit];
+            
+            game.renderLayers.background.filters = [depthLineGlow, bulgePinch, depthLineCRTFilter];
+            game.renderLayers.powerup.filters = [powerupGlow, advancedBloom, bulgePinch, powerupCRT, rgbSplit, powerupDropShadow, powerupMotionBlur];
+            game.renderLayers.powerupGlitched.filters = [powerupGlow, advancedBloom, bulgePinch, powerupCRT, rgbSplit, powerupDropShadow, powerupMotionBlur, powerdownGlitch];
+            game.renderLayers.powerdown.filters = [powerdownGlow, advancedBloom, bulgePinch, powerupCRT, rgbSplit, powerdownDropShadow, powerupMotionBlur, powerdownGlitch];
+            game.renderLayers.ballChange.filters = [ballChangeGlow, advancedBloom, bulgePinch, powerupCRT, rgbSplit, ballChangeDropShadow, powerupMotionBlur];
+            game.renderLayers.crossCut.filters = [powerupGlow, advancedBloom, bulgePinch, powerupCRT, rgbSplit, powerupDropShadow, powerupMotionBlur];
+        }
+
         this.addComponent(new PostProcessingComponent({
             advancedBloom: advancedBloom,
 			crtFilter: crtFilter,
@@ -199,12 +220,5 @@ export class PostProcessingLayer extends Entity {
             powerupCRT: powerupCRT,
             powerdownGlitch: powerdownGlitch,
         }));
-
-        game.renderLayers.background.filters = [depthLineGlow, bulgePinch, depthLineCRTFilter];
-        game.renderLayers.powerup.filters = [powerupGlow, advancedBloom, bulgePinch, powerupCRT, rgbSplit, powerupDropShadow, powerupMotionBlur];
-        game.renderLayers.powerupGlitched.filters = [powerupGlow, advancedBloom, bulgePinch, powerupCRT, rgbSplit, powerupDropShadow, powerupMotionBlur, powerdownGlitch];
-        game.renderLayers.powerdown.filters = [powerdownGlow, advancedBloom, bulgePinch, powerupCRT, rgbSplit, powerdownDropShadow, powerupMotionBlur, powerdownGlitch];
-        game.renderLayers.ballChange.filters = [ballChangeGlow, advancedBloom, bulgePinch, powerupCRT, rgbSplit, ballChangeDropShadow, powerupMotionBlur];
-        game.renderLayers.crossCut.filters = [powerupGlow, advancedBloom, bulgePinch, powerupCRT, rgbSplit, powerupDropShadow, powerupMotionBlur];
     }
 }
--- containers/frontend/src/pong/menu/menuEntities/MenuPostProcessingLayer.ts
+++ containers/frontend/src/pong/menu/menuEntities/MenuPostProcessingLayer.ts
@@ -6,7 +6,7 @@
 /*   By: hmunoz-g <hmunoz-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 17:47:20 by hmunoz-g          #+#    #+#             */
-/*   Updated: 2025/07/21 20:42:21 by hmunoz-g         ###   ########.fr       */
+/*   Updated: 2025/07/22 10:15:22 by hmunoz-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,75 +19,80 @@ import { PostProcessingComponent } from "../../components/PostProcessingComponen
 import { GAME_COLORS } from '../../utils/Types';
 
 export class MenuPostProcessingLayer extends Entity {
+    menu: Menu;
+    
     constructor(id: string, layer: string, menu: Menu) {
         super(id, layer);
+        this.menu = menu;
 
         const container = new Container();
         this.addComponent(new RenderComponent(container));
 
+        const isFirefox = this.menu.isFirefox;
+
         const advancedBloom = new AdvancedBloomFilter({
-            threshold: 0.7,
-            bloomScale: 0.2,
+            threshold: isFirefox ? 0.8 : 0.7,
+            bloomScale: isFirefox ? 0.15 : 0.2,
             brightness: 1,
-            blur: 1,
-            quality: 10,
-            pixelSize: 0.5,
+            blur: isFirefox ? 0.8 : 1,
+            quality: isFirefox ? 6 : 10,
+            pixelSize: isFirefox ? 0.8 : 0.5,
         });
 
         const crtFilter = new CRTFilter({
             curvature: (menu.width * 0.0005 + menu.height * 0.0005) / 2,
-            lineWidth: 0.05,
-            lineContrast: 0.15,
+            lineWidth: isFirefox ? 0.03 : 0.05,
+            lineContrast: isFirefox ? 0.1 : 0.15,
             verticalLine: false,
-            noise: 0.05,
-            noiseSize: 0.2,
+            noise: isFirefox ? 0.03 : 0.05,
+            noiseSize: isFirefox ? 0.3 : 0.2,
             seed: Math.random(),
-            vignetting: 0.45,
-            vignettingAlpha: 0.6,
-            vignettingBlur: 0.1,
+            vignetting: isFirefox ? 0.35 : 0.45,
+            vignettingAlpha: isFirefox ? 0.4 : 0.6,
+            vignettingBlur: isFirefox ? 0.05 : 0.1,
             time: 0
         });
 
          const crtOverlay = new CRTFilter({
             curvature: (menu.width * 0.0005 + menu.height * 0.0005) / 2,
-            lineWidth: 0.05,
-            lineContrast: 0.15,
+            lineWidth: isFirefox ? 0.03 : 0.05,
+            lineContrast: isFirefox ? 0.1 : 0.15,
             verticalLine: false,
-            noise: 0.05,
-            noiseSize: 0.2,
+            noise: isFirefox ? 0.03 : 0.05,
+            noiseSize: isFirefox ? 0.3 : 0.2,
             seed: Math.random(),
-            vignetting: 0.45,
-            vignettingAlpha: 0.6,
-            vignettingBlur: 0.1,
+            vignetting: isFirefox ? 0.35 : 0.45,
+            vignettingAlpha: isFirefox ? 0.4 : 0.6,
+            vignettingBlur: isFirefox ? 0.05 : 0.1,
             time: 0
         });
 
         const bulgePinch = new BulgePinchFilter({
             center: new Point(0.5, 0.5),
-            radius: Math.min(menu.width, menu.height) * 1.6,
-            strength: (1 / menu.width / menu.height) * 70000,
+            radius: Math.min(menu.width, menu.height) * (isFirefox ? 1.4 : 1.6),
+            strength: (1 / menu.width / menu.height) * (isFirefox ? 50000 : 70000),
         });
 
         const rgbSplit = new RGBSplitFilter({
-            red:   new Point(-0.5, 0),
-            green: new Point(0, 0.5),
-            blue:  new Point(0.5, -0.5),
+            red:   new Point(isFirefox ? -0.3 : -0.5, 0),
+            green: new Point(0, isFirefox ? 0.3 : 0.5),
+            blue:  new Point(isFirefox ? 0.3 : 0.5, isFirefox ? -0.3 : -0.5),
         });
 
         const glow = new GlowFilter({
-            alpha: 0.1,
+            alpha: isFirefox ? 0.08 : 0.1,
             color: GAME_COLORS.white,
-            distance: 5,
+            distance: isFirefox ? 3 : 5,
             innerStrength: 0,
             knockout: false,
-            outerStrength: 0.5,
-            quality: 0.1,
+            outerStrength: isFirefox ? 0.3 : 0.5,
+            quality: isFirefox ? 0.05 : 0.1,
         });
 
         const powerupCRT = new CRTFilter({
             curvature: (menu.width * 0.0005 + menu.height * 0.0005) / 2,
-            lineWidth: 0.1,
-            lineContrast: 0.2,
+            lineWidth: isFirefox ? 0.08 : 0.1,
+            lineContrast: isFirefox ? 0.15 : 0.2,
             verticalLine: false,
             noise: 0.,
             noiseSize: 0.5,
@@ -99,62 +104,62 @@ export class MenuPostProcessingLayer extends Entity {
         });
 
         const powerupGlow = new GlowFilter({
-            alpha: 0.2,
+            alpha: isFirefox ? 0.15 : 0.2,
             color: GAME_COLORS.marine,
-            distance: 10,
-            innerStrength: 3,
+            distance: isFirefox ? 7 : 10,
+            innerStrength: isFirefox ? 2 : 3,
             knockout: false,
-            outerStrength: 2,
-            quality: 0.1,
+            outerStrength: isFirefox ? 1.5 : 2,
+            quality: isFirefox ? 0.05 : 0.1,
         });
 
         const powerdownGlow = new GlowFilter({
-            alpha: 0.2,
+            alpha: isFirefox ? 0.15 : 0.2,
             color: GAME_COLORS.marine,
-            distance: 10,
-            innerStrength: 3,
+            distance: isFirefox ? 7 : 10,
+            innerStrength: isFirefox ? 2 : 3,
             knockout: false,
-            outerStrength: 2,
-            quality: 0.1,
+            outerStrength: isFirefox ? 1.5 : 2,
+            quality: isFirefox ? 0.05 : 0.1,
         });
 
         const ballChangeGlow = new GlowFilter({
-            alpha: 0.2,
+            alpha: isFirefox ? 0.15 : 0.2,
             color: GAME_COLORS.marine,
-            distance: 10,
-            innerStrength: 3,
+            distance: isFirefox ? 7 : 10,
+            innerStrength: isFirefox ? 2 : 3,
             knockout: false,
-            outerStrength: 2,
-            quality: 0.1,
+            outerStrength: isFirefox ? 1.5 : 2,
+            quality: isFirefox ? 0.05 : 0.1,
         });
 
         const powerupDropShadow = new DropShadowFilter({
-            alpha: 0.75,
-            blur: 1,
+            alpha: isFirefox ? 0.6 : 0.75,
+            blur: isFirefox ? 0.8 : 1,
             color: GAME_COLORS.green,
-            offset: {x: 4,y: 4},
-            pixelSize: {x:1,y:1},
-            quality: 4,
+            offset: {x: isFirefox ? 3 : 4, y: isFirefox ? 3 : 4},
+            pixelSize: {x: 1, y: 1},
+            quality: isFirefox ? 2 : 4,
             resolution: 1,
         });
         
         const powerdownDropShadow = new DropShadowFilter({
-            alpha: 0.75,
-            blur: 1,
+            alpha: isFirefox ? 0.6 : 0.75,
+            blur: isFirefox ? 0.8 : 1,
             color: GAME_COLORS.red,
-            offset: {x: 4,y: 4},
-            pixelSize: {x:1,y:1},
-            quality: 4,
+            offset: {x: isFirefox ? 3 : 4, y: isFirefox ? 3 : 4},
+            pixelSize: {x: 1, y: 1},
+            quality: isFirefox ? 2 : 4,
             resolution: 1,
         });
 
         const ballChangeDropShadow = new DropShadowFilter({
-            alpha: 0.75,
-            blur: 1,
+            alpha: isFirefox ? 0.6 : 0.75,
+            blur: isFirefox ? 0.8 : 1,
             color: GAME_COLORS.brown,
-            offset: {x: 4,y: 4},
-            pixelSize: {x:1,y:1},
-            quality: 4,
+            offset: {x: isFirefox ? 3 : 4, y: isFirefox ? 3 : 4},
+            pixelSize: {x: 1, y: 1},
+            quality: isFirefox ? 2 : 4,
             resolution: 1,
         });
 
@@ -165,10 +170,10 @@ export class MenuPostProcessingLayer extends Entity {
             red: {x: 0.5, y: 0.5},
             direction: 0,
             fillMode: 1,
-            offset : 2,
-            sampleSize: 512,
+            offset: isFirefox ? 1 : 2,
+            sampleSize: isFirefox ? 256 : 512,
             seed: 0,
-            slices: 200,
+            slices: isFirefox ? 100 : 200,
         });
 
         this.addComponent(new PostProcessingComponent({
@@ -180,11 +185,20 @@ export class MenuPostProcessingLayer extends Entity {
             rgbSpilt: rgbSplit,
             powerdownGlitch: powerdownGlitch,
         }));
-        menu.baseFilters = [glow, advancedBloom, bulgePinch, crtFilter, rgbSplit];
-        menu.powerupFilters = [powerupGlow, advancedBloom, bulgePinch, powerupCRT, rgbSplit, powerupDropShadow];
-        menu.powerdownFilters = [powerdownGlow, advancedBloom, bulgePinch, powerupCRT, rgbSplit, powerdownDropShadow, powerdownGlitch];
-        menu.ballchangeFilters = [ballChangeGlow, advancedBloom, bulgePinch, powerupCRT, rgbSplit, ballChangeDropShadow];
-        menu.powerupClassicFilters = [advancedBloom, bulgePinch, powerupCRT, rgbSplit];
+
+        if (isFirefox) {
+            menu.baseFilters = [glow, advancedBloom, crtFilter];
+            menu.powerupFilters = [powerupGlow, advancedBloom, powerupCRT, powerupDropShadow];
+            menu.powerdownFilters = [powerdownGlow, advancedBloom, powerupCRT, powerdownDropShadow];
+            menu.ballchangeFilters = [ballChangeGlow, advancedBloom, powerupCRT, ballChangeDropShadow];
+            menu.powerupClassicFilters = [advancedBloom, powerupCRT];
+        } else {
+            menu.baseFilters = [glow, advancedBloom, bulgePinch, crtFilter, rgbSplit];
+            menu.powerupFilters = [powerupGlow, advancedBloom, bulgePinch, powerupCRT, rgbSplit, powerupDropShadow];
+            menu.powerdownFilters = [powerdownGlow, advancedBloom, bulgePinch, powerupCRT, rgbSplit, powerdownDropShadow, powerdownGlitch];
+            menu.ballchangeFilters = [ballChangeGlow, advancedBloom, bulgePinch, powerupCRT, rgbSplit, ballChangeDropShadow];
+            menu.powerupClassicFilters = [advancedBloom, bulgePinch, powerupCRT, rgbSplit];
+        }
 
         menu.visualRoot.filters = menu.baseFilters;
         menu.menuContainer.filters = menu.baseFilters;
